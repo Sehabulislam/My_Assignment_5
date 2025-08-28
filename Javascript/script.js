3ecb03e3fe5fbcd1c27d89531697f5c8354318a0
Sehabulislam <shehabulislam244@gmail.com>
2025-08-29 01:12:56 +0600

    added the call alert and cut coin

--- Javascript/script.js
+++ Javascript/script.js
@@ -14,20 +14,35 @@ document.getElementById('card-container').addEventListener('click', function(e){
  })
 
 
-
+//call btn click and after event 
 getElement('card-container').addEventListener('click',function(e){
     if(e.target.className.includes('call-btn')){
         const callBtn = e.target;
         // Call btn er Title and Number 
         const callerTitle = callBtn.parentNode.parentNode.children[0].innerText;
         const callerNumber = callBtn.parentNode.parentNode.children[2].innerText;
+        
+        //Coin minus and not enough coin alert
+        const coin = getElement('coin').innerText;
+        const minusCoin = Number(coin) - 20;
+
         // Call alert 
+        if(minusCoin >= 0){
         alert("📞"+ " " + 'Calling ' + callerTitle + ' ' + callerNumber+ " " + '.....')
-        //Time set 
+        getElement('coin').innerText = minusCoin;
+        }
+        else {
+            alert("❌" + " " + "You don't have enough coins. You need at least 20 coins to make a call.")
+            return
+        }
+        
+        //Time set
         const time = new Date().toLocaleTimeString()
         // Call History Add 
+
         const callHistory = getElement('call-history');
 
+        //Add call history in Call History's
         const newCall = document.createElement('div');
         newCall.innerHTML = `
         <div class="bg-gray-100 h-auto p-4 mt-4 rounded-xl flex justify-between items-center">
@@ -36,7 +51,7 @@ getElement('card-container').addEventListener('click',function(e){
                     <p class="text-lg font-semibold text-gray-700">${callerNumber}</p>
                     </div>
                     <div>
-                        <p id="time" class="text-lg">${time}</p>
+                        <p id="time" class="text-md">${time}</p>
                     </div>
                   </div>
         `
